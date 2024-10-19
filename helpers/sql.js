0843057ecc9ae3bf63862f8cfeda1d753e23d64a
Tara Denniston <taradenniston@gmail.com>
2024-10-19 13:49:01 -0400

    add method description for sqlForPartialUpdate

--- helpers/sql.js
+++ helpers/sql.js
@@ -1,6 +1,18 @@
 const { BadRequestError } = require("../expressError");
 
-// THIS NEEDS SOME GREAT DOCUMENTATION.
+/** Create SQL to update whatever fields are provided in the data.
+ *
+ * dataToUpdate is a JS property that represents a SQL field;
+ * for example: {name, description, numEmployees, logoUrl}
+ *
+ * jsToSql: an object that matches up JS properties to SQL fields if they are 
+ * different; for example: { numEmployees: "num_employees", logoUrl: "logo_url" }
+ * 
+ * Returns object containing a string of column names and a string of new values;
+ * for example: { setCols: "name, num_employees", values: "Anderson and Morrow, 190" }
+ *
+ * Throws BadRequestError if no data is provided.
+ */
 
 function sqlForPartialUpdate(dataToUpdate, jsToSql) {
   const keys = Object.keys(dataToUpdate);
