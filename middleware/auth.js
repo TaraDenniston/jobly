d069b1942f31291ede2c63d115b2039d176aae4e
TaraDenniston <taradenniston@gmail.com>
2025-04-14 16:36:45 -0400

    add middleware for checking for either admin status or correct user

--- middleware/auth.js
+++ middleware/auth.js
@@ -58,9 +58,28 @@ function ensureAdmin(req, res, next) {
   }
 }
 
+/** Middleware to ensure the logged-in user is either:
+ * - the same user as the one in the route parameter, or
+ * - an admin.
+ *
+ * If not, raises Unauthorized.
+ */
+function ensureAdminOrUser(req, res, next) {
+  try {
+    const user = res.locals.user;
+
+    if (!user || (!user.isAdmin && user.username !== req.params.username)) {
+      throw new UnauthorizedError();
+    }
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+}
 
 module.exports = {
   authenticateJWT,
   ensureLoggedIn,
-  ensureAdmin
+  ensureAdmin,
+  ensureAdminOrUser
 };
