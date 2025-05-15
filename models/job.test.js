ce66ca1a9535a9fe5aa9ae188f44fc4efc0b79ce
TaraDenniston <taradenniston@gmail.com>
2025-05-15 10:16:00 -0400

    add tests for remove() method

--- models/job.test.js
+++ models/job.test.js
@@ -258,3 +258,22 @@ describe("update", function () {
     }
   });
 });
+
+/************************************** remove */
+describe("remove", function () {
+  test("works", async function () {
+    let t1Job = await Job.find({ titleLike: "t1" });
+    await Job.remove(t1Job[0].id);
+    const res = await db.query(
+        "SELECT id FROM jobs WHERE id=$1", [t1Job[0].id]);
+    expect(res.rows.length).toEqual(0);
+  });
+
+  test("not found if no such job", async function () {
+    try {
+      await Job.remove(0);
+    } catch (err) {
+      expect(err instanceof NotFoundError).toBeTruthy();
+    }
+  });
+});
