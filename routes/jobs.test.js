48f0e85ba7b7077831718049ddd6619557bcfcbe
TaraDenniston <taradenniston@gmail.com>
2025-05-19 11:41:39 -0400

    add tests for post route

---
+++ routes/jobs.test.js
@@ -0,0 +1,78 @@
+"use strict";
+
+const request = require("supertest");
+
+const db = require("../db");
+const app = require("../app");
+
+const {
+  commonBeforeAll,
+  commonBeforeEach,
+  commonAfterEach,
+  commonAfterAll,
+  u1Token,
+  u2Token
+} = require("./_testCommon");
+
+beforeAll(commonBeforeAll);
+beforeEach(commonBeforeEach);
+afterEach(commonAfterEach);
+afterAll(commonAfterAll);
+
+/************************************** POST /jobs */  
+
+describe("POST /jobs", function () {
+  const newJob = {
+    title: "New",
+    salary: 90000,
+    equity: 0.01,
+    companyHandle: "c3",
+  };
+
+  test("ok for admins", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(201);
+    expect(resp.body).toEqual({
+      job: {
+        id: expect.any(Number),
+        title: "New",
+        salary: 90000,
+        equity: "0.01",
+        companyHandle: "c3"
+      }
+    });
+  });
+
+  test("unauth for non-admins", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send(newJob)
+        .set("authorization", `Bearer ${u2Token}`);
+    expect(resp.statusCode).toEqual(401);
+  });
+
+  test("bad request with missing data", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send({
+          title: "New",
+          salary: 100000,
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+
+  test("bad request with invalid data", async function () {
+    const resp = await request(app)
+        .post("/jobs")
+        .send({
+          ...newJob,
+          equity: "not-a-number",
+        })
+        .set("authorization", `Bearer ${u1Token}`);
+    expect(resp.statusCode).toEqual(400);
+  });
+});
