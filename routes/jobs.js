411647ca98bd4118434d0dba2797e336cb1fb201
TaraDenniston <taradenniston@gmail.com>
2025-05-22 19:56:52 -0400

    add DELETE route for jobs/:id

--- routes/jobs.js
+++ routes/jobs.js
@@ -112,6 +112,17 @@ router.patch("/:id", ensureAdmin, async function (req, res, next) {
   }
 });
 
-
+/** DELETE /[id]  =>  { deleted: id }
+ *
+ * Authorization required: admin
+ */
+router.delete("/:id", ensureAdmin, async function (req, res, next) {
+  try {
+    await Job.remove(req.params.id);
+    return res.json({ deleted: req.params.id });
+  } catch (err) {
+    return next(err);
+  }
+});
 
 module.exports = router;
